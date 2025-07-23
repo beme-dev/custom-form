87dee3267c1e8a1d1fde9e0b26670607c5797fb9
chlbri <51156116+chlbri@users.noreply.github.com>
2025-07-23 15:36:41 +0000

    Refactor test structure and data management
    - Removed outdated test files and CSV fixtures from `src/__tests__`.
    - Added new test data CSV files to `src/fixtures/csv` for better organization.
    - Created a constants file in `src/fixtures` to manage file paths.
    - Implemented a helper function for resolving file paths in `src/fixtures/helpers.ts`.
    - Updated end-to-end tests in `src/routes/-form.e2e.ts` to utilize new file structure and constants.
    - Added a new README for the fixtures directory to document the test structure and execution instructions.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig, devices } from '@playwright/test';
 
-const PORT = 5000;
+const PORT = 5172;
 
 export default defineConfig({
-  testDir: './src/__tests__',
   // fullyParallel: true,
+  testDir: './src/routes',
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
+  testMatch: '**/*.@(e2e).?(c|m)[jt]s?(x)',
+  maxFailures: process.env.CI ? 1 : 3,
+
   use: {
     baseURL: `http://localhost:${PORT}`,
     trace: 'on-first-retry',
@@ -17,11 +20,36 @@ export default defineConfig({
   expect: {
     timeout: 10 * 1000, // 10 seconds
   },
+
   // Configure projects for major browsers
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge'] },
+    },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Galaxy A55 landscape'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 15 Pro Max'] },
+    },
+    {
+      use: { ...devices['iPad (gen 11)'] },
+      name: 'iPad',
     },
   ],
   webServer: {
